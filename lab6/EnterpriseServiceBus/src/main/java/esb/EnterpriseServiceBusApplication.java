bca28ce08f834f67fe2d9d5b0bc11b6c634c3530
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2025-08-01 20:28:21 -0500

    porting legacy project to modern java

---
+++ lab6/EnterpriseServiceBus/src/main/java/esb/EnterpriseServiceBusApplication.java
@@ -0,0 +1,13 @@
+package esb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseServiceBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseServiceBusApplication.class, args);
+    }
+
+}
