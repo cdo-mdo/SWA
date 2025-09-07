74398fad01fee485ef8fcf27c9fb252569c56bb2
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2025-09-07 10:58:41 -0500

    add lab 10

---
+++ Lab10/ConfigServer/src/main/java/cs590de/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package cs590de.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
---
+++ Lab10/ServiceB/src/main/java/cs590de/serviceb/controller/ServiceBController.java
@@ -0,0 +1,23 @@
+package cs590de.serviceb.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceBController {
+
+    @Value("${greeting}")
+    private String greeting;
+
+    @Value("${message}")
+    private String message;
+
+    @RequestMapping("/")
+    public String getName() {
+        return message+" , "+greeting;
+    }
+
+}
+
