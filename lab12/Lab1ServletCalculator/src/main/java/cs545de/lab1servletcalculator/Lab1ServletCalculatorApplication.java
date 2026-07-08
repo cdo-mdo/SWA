cce3c1187a6ffa58934f7b71e4af4fcdef45300a
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2026-07-08 17:38:57 -0500

    Add lab-12

---
+++ lab12/Lab1ServletCalculator/src/main/java/cs545de/lab1servletcalculator/Lab1ServletCalculatorApplication.java
@@ -0,0 +1,13 @@
+package cs545de.lab1servletcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1ServletCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1ServletCalculatorApplication.class, args);
+	}
+
+}
